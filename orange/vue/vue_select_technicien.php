c9efba891a580677047e1edcda060e462e6f4285
Roro9526 <144102289+Roro9526@users.noreply.github.com>
2023-10-30 10:44:23 +0100

    Add files via upload

---
+++ orange/vue/vue_select_technicien.php
@@ -0,0 +1,32 @@
+<h3> Liste des Techniciens</h3>
+<form method="post">
+    Filtrer par : <input type="text" name="filtre">
+    <input type="submit" name="Filtrer" value="Filtrer">
+</form>
+<br/>
+<table border="1">
+    <tr>
+        <td>Id Technicien</td>
+        <td>Nom du Technicien</td>
+        <td>Prenom du technicien</td>
+        <td>Qualification</td>
+        <td>Email</td>
+        <td>Opération </td>
+    </tr>
+    <?php
+    if (isset($lestechniciens)){
+        foreach ($lestechniciens as $untechnicien){
+            echo "<tr>";
+            echo "<td>" . $untechnicien['idtech'] ."</td>";
+            echo "<td>" . $untechnicien['nom'] ."</td>";
+            echo "<td>" . $untechnicien['prenom'] ."</td>";
+            echo "<td>" . $untechnicien['qualification'] ."</td>";
+            echo "<td>" . $untechnicien['email'] ."</td>";
+            echo "<td>";
+            echo "<a href='index.php?page=4&action=sup&idclasse=".$untechnicien['idtech']."'><img src='images/supprimer.png' height='30' width='30'></a>";
+            echo "<a href='index.php?page=4&action=edit&idclasse=".$untechnicien['idtech']."'><img src='images/editer.png' height='30' width='30'></a>";
+            echo "</td>";
+            echo"</td>";
+        }
+    } ?>
+</table>
