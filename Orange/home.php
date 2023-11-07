975350525403ab7065e446f337399f5a8b179b6d
Christina GAPY <114501721+christinaa0@users.noreply.github.com>
2023-11-07 13:42:16 +0100

    Add files via upload

---
+++ Orange/home.php
@@ -0,0 +1,11 @@
+<br>
+<h2> Orange</h2>
+<br>
+<h3>Bienvenue : <?= $_SESSION['nom']." ".$_SESSION['prenom']?> </h3>
+<h3> Vous êtes connecter en tant que : <?=$_SESSION['role']?></h3>
+<br>
+<br>
+<br>
+<img src="img/orange.png" height="700" width="1500">
+<br>
+<br>
